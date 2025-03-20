55412189fd8fe64c0881deb5e45756f1c10636af
Georgios11 <tsompanidis.georgios@gmail.com>
2025-03-20 10:57:42 +0100

    data fetched with query and axios

--- react-typescript/src/starter/08-fetch-data/index.tsx
+++ react-typescript/src/starter/08-fetch-data/index.tsx
@@ -1,62 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useState, useEffect } from "react";
-import { Tour, tourSchema } from "./types";
+import { fetchTours } from "./types";
+import { useQuery } from "@tanstack/react-query";
+
 function Component() {
-	const url: string = "https://www.course-api.com/react-tours-project";
-
-	const [isLoading, setIsLoading] = useState(false);
-	const [isError, setIsError] = useState<string | null>(null);
-	const [tours, setTours] = useState<Tour[]>([]);
-	useEffect(() => {
-		const fetchData = async () => {
-			setIsLoading(true);
-			try {
-				const response = await fetch(url);
-				if (!response.ok) {
-					throw new Error(`Failed to fetch data...`);
-				}
-				const rawData: Tour[] = await response.json();
-				const result = tourSchema.array().safeParse(rawData);
-
-				if (!result.success) {
-					console.log(result.error.message);
-					throw new Error(`Failed to parse tours`);
-				}
-				setTours(result.data);
-			} catch (error) {
-				const message =
-					error instanceof Error
-						? error.message
-						: "Something went wrong";
-				setIsError(message);
-				console.log(message);
-			} finally {
-				setIsLoading(false);
-			}
-		};
-		fetchData();
-	}, []);
-	if (isLoading) {
-		return <h2>Loading...</h2>;
-	}
-	if (isError) {
-		return <h2>Error {isError}</h2>;
-	}
-
+	const {
+		isPending,
+		isError,
+		error,
+		data: tours,
+	} = useQuery({ queryKey: ["tours"], queryFn: fetchTours });
+	if (isPending) return <h2>Loading...</h2>;
+	if (isError) return <h2>Error : {error.message}</h2>;
 	return (
 		<div>
-			<h2>React & Typescript</h2>
-
-			<div>
-				<h2 className="mb-1">Tours</h2>
-				{tours.map((tour: Tour) => {
-					return (
-						<p key={tour.id} className="mb-1">
-							{tour.name}
-						</p>
-					);
-				})}
-			</div>
+			<h2 className="mb-1">Tours</h2>
+			{tours.map((tour) => {
+				return (
+					<p className="mb-1" key={tour.id}>
+						{tour.name}
+					</p>
+				);
+			})}
 		</div>
 	);
 }
--- react-typescript/src/starter/08-fetch-data/types.ts
+++ react-typescript/src/starter/08-fetch-data/types.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { z } from "zod";
-
+import axios from "axios";
+const url: string = "https://www.course-api.com/react-tours-project";
 export const tourSchema = z.object({
 	id: z.string(),
 	name: z.string(),
@@ -9,3 +11,14 @@ export const tourSchema = z.object({
 	// something: z.string(),
 });
 export type Tour = z.infer<typeof tourSchema>;
+
+export const fetchTours = async (): Promise<Tour[]> => {
+	const response = await axios.get<Tour[]>(url);
+	const result = tourSchema.array().safeParse(response.data);
+
+	if (!result.success) {
+		console.log(result.error.message);
+		throw new Error(`Failed to parse tours`);
+	}
+	return result.data;
+};
